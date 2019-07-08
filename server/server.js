4ea9ccffa1345d468602a8418f68c77919e555bf
Ryan Moore <rymoore411@gmail.com>
2019-07-07 21:54:09 -0700

    basic setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, '../client/index.html')));
+
