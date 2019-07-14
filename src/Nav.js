b33eb888ef46593d85466fc130d28aec4c322768
Ryan Moore <rymoore411@gmail.com>
2019-07-14 14:26:27 -0700

    working on forms

--- src/Nav.js
+++ src/Nav.js
@@ -6,9 +6,42 @@ import { Link } from 'react-router-dom';
 const _Nav = ({ schools, students})=> {
   return(
     <div>
+    <div>
       <Link to='/'>Home</Link>
       <Link to='/schools'>Schools ({schools.length})</Link>
       <Link to='/students'>Students ({students.length})</Link>
+    </div>
+    <div>
+      <form>
+        <div>
+          <label>First Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>GPA</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>Enroll at</label>
+          <select name="schools">
+            {
+              schools.map((school)=>(
+                <option key={school.id}>{school.name}</option>
+              ))
+            }
+          </select>
+        </div>
+        <button>Save</button>
+      </form>
+    </div>
     </div>
   );
 }
